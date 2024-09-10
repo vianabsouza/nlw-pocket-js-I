108dc2eacb64663bf59852285c5c49cd249a9bf4
Barbara Viana <vianabsouza@gmail.com>
2024-09-10 14:11:27 -0300

    feat: create goal registration

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const { select } = require('@inquirer/prompts');
+const { select, input } = require('@inquirer/prompts');
+
+let meta = {
+  value: "Tomar 2L de água por dia",
+  checked: false,
+}
+
+let metas = [ meta ];
+const cadastrarMeta = async () => {
+  const meta = await input({ message: "Digite a meta:" })
+
+  if(meta.length == 0) {
+    console.log("A meta não pode ser vazia.")
+    return
+  }
+
+  metas.push({
+    value: meta,
+    checked: false
+  });
+}
 
 const start = async () => {
   while (true) {
@@ -22,7 +42,8 @@ const start = async () => {
 
     switch(option) {
       case "Cadastrar":
-        console.log("Cadastrando...")
+        await cadastrarMeta();
+        console.log(metas);
         break;
       case "Listar":
         console.log("Listando...")
